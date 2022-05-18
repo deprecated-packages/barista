af00027ba5cc91c81495be87a451246f78603c52
Tomas Votruba <tomas.vot@gmail.com>
2022-05-18 16:18:21 +0200

    add LatteFilesFinder

---
+++ src/FileSystem/LatteFilesFinder.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barista\FileSystem;
+
+use Nette\Utils\Finder;
+use Symplify\SmartFileSystem\FileSystemFilter;
+
+/**
+ * @see \Barista\Tests\FileSystem\LatteFilesFinder\LatteFilesFinderTest
+ */
+final class LatteFilesFinder
+{
+    public function __construct(
+        private FileSystemFilter $fileSystemFilter
+    ) {
+    }
+
+    /**
+     * @param string[] $filesOrDirectories
+     * @return \SplFileInfo[]
+     */
+    public function find(array $filesOrDirectories): array
+    {
+        $directories = $this->fileSystemFilter->filterDirectories($filesOrDirectories);
+        $files = $this->fileSystemFilter->filterFiles($filesOrDirectories);
+
+        $finder = Finder::findFiles('*.latte')
+            ->in(...$directories);
+
+        $foundFileInfos = iterator_to_array($finder);
+        foreach ($files as $file) {
+            $foundFileInfos[] = new \SplFileInfo($file);
+        }
+
+        return $foundFileInfos;
+    }
+}
---
+++ tests/FileSystem/LatteFilesFinder/LatteFilesFinderTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Barista\Tests\FileSystem\LatteFilesFinder;
+
+use Barista\DI\BaristaContainerFactory;
+use Barista\FileSystem\LatteFilesFinder;
+use PHPUnit\Framework\TestCase;
+
+final class LatteFilesFinderTest extends TestCase
+{
+    private LatteFilesFinder $latteFilesFinder;
+
+    protected function setUp(): void
+    {
+        $baristaContainerFactory = new BaristaContainerFactory();
+        $container = $baristaContainerFactory->create();
+
+        $this->latteFilesFinder = $container->getByType(LatteFilesFinder::class);
+    }
+
+    public function test(): void
+    {
+        $foundLatteFiles = $this->latteFilesFinder->find([
+            __DIR__ . '/Fixture/bare-file.latte',
+            __DIR__ . '/Fixture/another-dir',
+        ]);
+
+        $this->assertCount(2, $foundLatteFiles);
+        $this->assertContainsOnlyInstancesOf(\SplFileInfo::class, $foundLatteFiles);
+    }
+}
