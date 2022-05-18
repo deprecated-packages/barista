c79b897e45cf577e5dac2d943b2b63c6544c2a73
Tomas Votruba <tomas.vot@gmail.com>
2022-05-18 16:50:18 +0200

    make use of ApplicationFactory

--- bin/barista.php
+++ bin/barista.php
@@ -4,32 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Barista\Analyzer\LatteAnalyzer;
 use Barista\DI\BaristaContainerFactory;
+use Symfony\Component\Console\Application;
 
 $baristaContainerFactory = new BaristaContainerFactory();
 $container = $baristaContainerFactory->create();
 
-$commandNames = $container->findByType(\Symfony\Component\Console\Command\Command::class);
-$commands = [];
-foreach ($commandNames as $commandName) {
-    $commands[] = $container->getByName($commandName);
-}
-
-$application = new \Symfony\Component\Console\Application();
-$application->addCommands($commands);
+$application = $container->getByType(Application::class);
 $application->run();
-
-//$latteAnalyzer = $container->getByType(LatteAnalyzer::class);
-//
-//$inputArgs = $argv;
-//array_shift($inputArgs);
-//
-//$filePaths = $inputArgs;
-//
-//if ($filePaths === []) {
-//    exit('Provide file/dir paths to analyze' . PHP_EOL);
-//}
-//
-//$result = $latteAnalyzer->run($filePaths);
-//exit($result);
---
+++ src/Console/ApplicationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barista\Console;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+
+final class ApplicationFactory
+{
+    /**
+     * @param Command[] $commands
+     */
+    public function __construct(private array $commands)
+    {
+    }
+
+    public function create(): Application
+    {
+        $application = new Application();
+        $application->addCommands($this->commands);
+
+        return $application;
+    }
+}
