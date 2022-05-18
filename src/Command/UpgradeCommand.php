6597c106992e83888cf09b997def29cc2fed4228
Tomas Votruba <tomas.vot@gmail.com>
2022-05-18 12:45:58 +0200

    into command

---
+++ src/Command/UpgradeCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barista\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+final class UpgradeCommand extends Command
+{
+    // 1) @take paths
+    // 2) run upgrader
+    // 3) dump or dry-run :)
+}
